ebb6f8ed64c4229f776f4fdc5c3dcb1473c639bd
wkdewey <wkdewey@gmail.com>
2020-09-11 10:52:22 -0400

    add method to fetch an individual monastery

--- src/index.js
+++ src/index.js
@@ -4,10 +4,19 @@ function fetchMonasteries() {
     .then((response) => response.json())
     .then((json) => renderMonasteries(json));
 }
+function fetchMonastery(id) {
+  return fetch(`${BACKEND_URL}/api/v1/monasteries/${id}`)
+    .then((response) => response.json())
+    .then((json) => renderMonastery(json));
+}
 
 function renderMonasteries(json) {
   console.log(json);
 }
+function renderMonastery(json) {
+  console.log(json);
+}
 document.addEventListener("DOMContentLoaded", function () {
   fetchMonasteries();
+  fetchMonastery(1);
 });
