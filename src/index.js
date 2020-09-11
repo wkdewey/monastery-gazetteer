084a0bca26816a71212aa6b33c73a0e30c99c14d
wkdewey <wkdewey@gmail.com>
2020-09-11 10:34:31 -0400

    add method to fetch monastery data, log to console

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 const BACKEND_URL = "http://localhost:3000";
-fetch(`${BACKEND_URL}/test`)
-  .then((response) => response.json())
-  .then((parsedResponse) => console.log(parsedResponse));
+function fetchMonasteries() {
+  return fetch(`${BACKEND_URL}/api/v1/monasteries`)
+    .then((response) => response.json())
+    .then((json) => renderMonasteries(json));
+}
+
+function renderMonasteries(json) {
+  console.log(json);
+}
+document.addEventListener("DOMContentLoaded", function () {
+  fetchMonasteries();
+});
