16b080ac59c71762a421f16fd9294d421dffb83a
wkdewey <wkdewey@gmail.com>
2020-09-27 15:32:41 -0400

    create generic showForm model and move createInputElement into BuddhistEntity

--- src/buddhistentity.js
+++ src/buddhistentity.js
@@ -38,4 +38,56 @@ class BuddhistEntity {
     contentContainer.textContent = "";
     this.render();
   }
+  static showForm(model) {
+    const h2 = document.createElement("h2");
+    const form = document.createElement("form");
+    const br = document.createElement("br");
+    const contentContainer = document.querySelector("#content-container");
+    contentContainer.textContent = "";
+    contentContainer.appendChild(form);
+    form.appendChild(h2);
+    form.classList.add("d-flex", "flex-column", "align-items-center");
+    const inputName = this.createInputElement(
+      "input-name",
+      "text",
+      "name",
+      "",
+      `Enter ${model} name`
+    );
+    form.appendChild(inputName);
+    form.appendChild(br);
+    const inputTradition = this.createInputElement(
+      "input-religious-tradition",
+      "text",
+      "religious-tradition",
+      "",
+      "Enter religious tradition"
+    );
+    form.appendChild(inputTradition);
+    form.appendChild(br.cloneNode());
+    //get all figures, iterate through them, put their name and id
+    //means I need a function that just returns all the figures
+    const h3 = document.createElement("h3");
+    h3.textContent = `Choose Associated ${
+      model === "figure" ? "Monasteries" : "Figures"
+    }`;
+    form.appendChild(h3);
+    const submit = Helpers.createInputElement(
+      "create-button",
+      "submit",
+      "submit",
+      `Create new ${model}`
+    );
+    submit.classList.add("btn", "btn-sm", "btn-outline-secondary");
+    form.appendChild(submit);
+  }
+  static createInputElement(id, type, name, value, placeholder) {
+    const element = document.createElement("input");
+    element.id = id;
+    element.type = type;
+    element.name = name;
+    element.value = value;
+    element.placeholder = placeholder;
+    return element;
+  }
 }
