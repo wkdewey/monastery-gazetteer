781843e633b7046eac981c16dfbeba95df7d8d31
wkdewey <wkdewey@gmail.com>
2020-10-14 14:30:09 -0400

    fetch from Rails API

--- src/actions/placeActions.js
+++ src/actions/placeActions.js
@@ -1,5 +1,9 @@
 export const fetchPlaces = () => {
   return (dispatch) => {
     dispatch({ type: "LOADING_PLACES" });
+    fetch("http://localhost:3000/api/v1/places").then((response) => {
+      console.log(response.json());
+      return response.json();
+    });
   };
 };
