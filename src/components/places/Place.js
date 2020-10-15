110ed916c8236059a2986228da6a862ecc646b19
wkdewey <wkdewey@gmail.com>
2020-10-15 17:23:56 -0400

    add percentages of ancestry groups

--- src/components/places/Place.js
+++ src/components/places/Place.js
@@ -8,6 +8,9 @@ const Place = (props) => {
     <div className="place">
       <h3>{place.attributes.name}</h3>
       <p>Population: {place.attributes.population}</p>
+      <p>German-American: {place.attributes.percent_german}%</p>
+      <p>African-American: {place.attributes.percent_african_american}%</p>
+      <p>Mexican-American: {place.attributes.percent_mexican}%</p>
     </div>
   ) : "Place not found";
   return (
