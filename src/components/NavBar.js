2201e7505fc0a82ec15a131872dc94f9df1cce74
wkdewey <wkdewey@gmail.com>
2020-10-15 11:13:57 -0400

    create NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const NavBar = () => {
+  <div className="navbar">
+    <NavLink to="/">Home</NavLink>
+    <NavLink to="/places">Places</NavLink>
+  </div>
+}
