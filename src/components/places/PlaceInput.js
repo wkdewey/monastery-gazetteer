8b26f7ebb83dd29463e8a15590a06bd8ffe28043
wkdewey <wkdewey@gmail.com>
2020-10-15 20:31:13 -0400

    create PlaceInput with constructor

---
+++ src/components/places/PlaceInput.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class PlaceInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      population: 0,
+      german_pop: 0,
+      af_am_pop: 0,
+      mexican_pop: 0
+    }
+  }
+}
