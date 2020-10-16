182a2f98d79cc7057122f98bc4a07fff4d08fff2
wkdewey <wkdewey@gmail.com>
2020-10-16 17:34:15 -0400

    prettify, add onChange methods for name and population

--- src/components/places/PlaceInput.js
+++ src/components/places/PlaceInput.js
@@ -9,27 +9,91 @@ class PlaceInput extends Component {
       population: 0,
       german_pop: 0,
       af_am_pop: 0,
-      mexican_pop: 0
-    }
+      mexican_pop: 0,
+    };
   }
 
-  
+  handleNameChange = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+
+  handlePopulationChange = (event) => {
+    this.setState({
+      population: event.target.value,
+    });
+  };
 
   render() {
-    console.log("in PlaceInput")
+    console.log("in PlaceInput");
     return (
       <div>
-      <form>
-        <div><label>Place name<input id="name" name="name" type="text" value={this.state.name} /></label></div>
-        <div><label>Total population<input id="population" name="population" type="number" value={this.state.population} /></label></div>
-        <div><label>German-American population<input id="population" name="german_pop" type="number" value={this.state.german_pop} /></label></div>
-        <div><label>African-American population<input id="population" name="af_am_pop" type="number" value={this.state.af_am_pop} /></label></div>
-        <div><label>Mexican-American population<input id="population" name="mexican_pop" type="number" value={this.state.mexican_pop} /></label></div>
-        <div><button type="submit"> Submit</button></div>
-      </form>
+        <form>
+          <div>
+            <label>
+              Place name
+              <input
+                id="name"
+                name="name"
+                type="text"
+                value={this.state.name}
+                onChange={this.handleNameChange}
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              Total population
+              <input
+                id="population"
+                name="population"
+                type="number"
+                value={this.state.population}
+                onChange={this.state.handlePopulationChange}
+              />
+            </labqel>
+          </div>
+          <div>
+            <label>
+              German-American population
+              <input
+                id="population"
+                name="german_pop"
+                type="number"
+                value={this.state.german_pop}
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              African-American population
+              <input
+                id="population"
+                name="af_am_pop"
+                type="number"
+                value={this.state.af_am_pop}
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              Mexican-American population
+              <input
+                id="population"
+                name="mexican_pop"
+                type="number"
+                value={this.state.mexican_pop}
+              />
+            </label>
+          </div>
+          <div>
+            <button type="submit"> Submit</button>
+          </div>
+        </form>
       </div>
-    )
+    );
   }
 }
 
-export default PlaceInput
+export default PlaceInput;
