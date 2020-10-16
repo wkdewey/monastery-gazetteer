9832ac363083d68277a1a0bb8a2d118f4d482e3f
wkdewey <wkdewey@gmail.com>
2020-10-16 09:18:58 -0400

    create input form

--- src/components/places/PlaceInput.js
+++ src/components/places/PlaceInput.js
@@ -12,4 +12,17 @@ class PlaceInput extends React.Component {
       mexican_pop: 0
     }
   }
+
+  render() {
+    return (
+      <form>
+        <div><label>Place name<input id="name" name="name" type="text" value={this.state.name} /></label></div>
+        <div><label>Total population<input id="population" name="population" type="number" value={this.state.population} /></label></div>
+        <div><label>German-American population<input id="population" name="german_pop" type="number" value={this.state.german_pop} /></label></div>
+        <div><label>African-American population<input id="population" name="af_am_pop" type="number" value={this.state.af_am_pop} /></label></div>
+        <div><label>Mexican-American population<input id="population" name="mexican_pop" type="number" value={this.state.mexican_pop} /></label></div>
+        <div><button type="submit"> Submit</button></div>
+      </form>
+    )
+  }
 }
