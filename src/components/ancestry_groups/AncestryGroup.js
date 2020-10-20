f9f4b3f1b98ff37736bff711c72a711aaf80322a
wkdewey <wkdewey@gmail.com>
2020-10-20 11:32:22 -0400

    refactor AncestryGroup component and add PlaceComparison component

--- src/components/ancestry_groups/AncestryGroup.js
+++ src/components/ancestry_groups/AncestryGroup.js
@@ -6,10 +6,24 @@ const AncestryGroup = (props) => {
   let { ancestryGroupId } = useParams();
   console.log(useParams());
   const ancestry_group = props.ancestry_groups[ancestryGroupId - 1];
+  const places = ancestry_group.attributes.place_ancestry_groups;
+  places.sort((a, b) => (a.attributes.percent < b.attributes.percent ? 1 : -1));
   const ancestryGroupCard = ancestry_group ? (
-    <div className="place">
+    <div className="ancestry group">
       <h3>{ancestry_group.attributes.name}</h3>
-      <p>Population: {ancestry_group.attributes.national_pop}</p>
+      <p>
+        Population in the United States:{" "}
+        {ancestry_group.attributes.national_pop}
+      </p>
+      {places.map((place) => {
+        return (
+          <div className="place" key={place.id}>
+            <p>
+              {place.attributes.place.name}: {place.attributes.percent}%
+            </p>
+          </div>
+        );
+      })}
     </div>
   ) : (
     "Place not found"
