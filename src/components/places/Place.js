ea988563a16bafa43a4b0ee79a740c3af838cf60
wkdewey <wkdewey@gmail.com>
2020-10-20 10:32:33 -0400

    refactor to sort groups by percent

--- src/components/places/Place.js
+++ src/components/places/Place.js
@@ -4,13 +4,15 @@ import { useParams } from "react-router-dom";
 const Place = (props) => {
   console.log(props);
   let { placeId } = useParams();
-  console.log(useParams());
   const place = props.places[placeId - 1];
+  console.log(place);
+  const groups = place.attributes.place_ancestry_groups;
+  groups.sort((a, b) => (a.attributes.percent < b.attributes.percent ? 1 : -1));
   const placeCard = place ? (
     <div className="place">
       <h3>{place.attributes.name}</h3>
       <p>Population: {place.attributes.population}</p>
-      {place.attributes.place_ancestry_groups.map((group) => {
+      {groups.map((group) => {
         console.log(group);
         return (
           <div className="ancestry group" key={group.id}>
