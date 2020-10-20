5aeeba1325ebb3efcfd540b9f396171e0e5640f1
wkdewey <wkdewey@gmail.com>
2020-10-20 10:25:07 -0400

    iterate through associated ancestry groups and display name and percent

--- src/components/places/Place.js
+++ src/components/places/Place.js
@@ -10,9 +10,17 @@ const Place = (props) => {
     <div className="place">
       <h3>{place.attributes.name}</h3>
       <p>Population: {place.attributes.population}</p>
-      <p>German-American: {place.attributes.percent_german}%</p>
-      <p>African-American: {place.attributes.percent_african_american}%</p>
-      <p>Mexican-American: {place.attributes.percent_mexican}%</p>
+      {place.attributes.place_ancestry_groups.map((group) => {
+        console.log(group);
+        return (
+          <div className="ancestry group" key={group.id}>
+            <p>
+              {group.attributes.ancestry_group.name}: {group.attributes.percent}
+              %
+            </p>
+          </div>
+        );
+      })}
     </div>
   ) : (
     "Place not found"
