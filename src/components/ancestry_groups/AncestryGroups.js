721432ac3e56fa90d5a685677b99882141c732f8
wkdewey <wkdewey@gmail.com>
2020-10-22 16:28:11 -0400

    add loading message to AncestryGroups container

--- src/components/ancestry_groups/AncestryGroups.js
+++ src/components/ancestry_groups/AncestryGroups.js
@@ -1,23 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const AncestryGroups = (props) =>
-  props.ancestryGroups.map((ancestryGroup) => {
+const AncestryGroups = (props) => {
+  if (props.ancestryGroups.length > 0) {
+    return props.ancestryGroups.map((ancestryGroup) => {
+      return (
+        <div className="ancestry_group" key={ancestryGroup.id}>
+          <h3>
+            <Link
+              key={ancestryGroup.id}
+              to={`/ancestry_groups/${ancestryGroup.id}`}
+            >
+              {ancestryGroup.attributes.name}
+            </Link>
+          </h3>
+          <p>
+            Percent of US Population:{" "}
+            {ancestryGroup.attributes.national_percent}
+          </p>
+        </div>
+      );
+    });
+  } else {
     return (
-      <div className="ancestry_group" key={ancestryGroup.id}>
-        <h3>
-          <Link
-            key={ancestryGroup.id}
-            to={`/ancestry_groups/${ancestryGroup.id}`}
-          >
-            {ancestryGroup.attributes.name}
-          </Link>
-        </h3>
-        <p>
-          Percent of US Population: {ancestryGroup.attributes.national_percent}
-        </p>
+      <div>
+        <h3>Loading, please wait.</h3>
       </div>
     );
-  });
+  }
+};
 
 export default AncestryGroups;
