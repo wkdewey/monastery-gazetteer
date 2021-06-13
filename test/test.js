13b5df933c1209db57be915b6c69f2bd00a0ec0d
wkdewey <wkdewey@gmail.com>
2021-06-13 14:52:02 -0400

    test for properties

--- test/test.js
+++ test/test.js
@@ -1,16 +1,28 @@
 var assert = require("chai").assert;
 BuddhistEntity = require("../src/buddhistentity");
 describe("BuddhistEntity", function () {
+  testEntity = new BuddhistEntity(
+    "1",
+    "Buddha",
+    "Buddhism",
+    "https://example.com/buddha.jpg"
+  );
   describe("#constructor()", function () {
-    it("should return an instance of class BuddhistEntity", function () {
-      testEntity = new BuddhistEntity(
-        "1",
-        "Buddha",
-        "Buddhism",
-        "https://example.com/buddha.jpg"
-      );
+    it("returns an instance of class BuddhistEntity", function () {
       assert.typeOf(testEntity, "Object");
       assert.instanceOf(testEntity, BuddhistEntity);
+    });
+    it("has property id", function () {
+      assert.property(testEntity, "id");
+    });
+    it("has property name", function () {
+      assert.property(testEntity, "name");
+    });
+    it("has property religious_tradition", function () {
+      assert.property(testEntity, "religious_tradition");
+    });
+    it("has property image_url", function () {
+      assert.property(testEntity, "image_url");
     });
   });
 });
