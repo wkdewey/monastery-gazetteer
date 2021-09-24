111e7dc1d1c1658315093a7742578c4026360283
wkdewey <william.dewey@unl.edu>
2021-09-24 14:41:30 -0500

    Revert "test for properties"
    This reverts commit 13b5df933c1209db57be915b6c69f2bd00a0ec0d.

--- test/test.js
+++ test/test.js
@@ -13,21 +13,15 @@ describe("BuddhistEntity", function () {
   const BACKEND_URL = "https://young-forest-03120.herokuapp.com";
   const MONASTERIES_URL = `${BACKEND_URL}/api/v1/monasteries`;
   describe("#constructor()", function () {
-    it("returns an instance of class BuddhistEntity", function () {
+    it("should return an instance of class BuddhistEntity", function () {
+      testEntity = new BuddhistEntity(
+        "1",
+        "Buddha",
+        "Buddhism",
+        "https://example.com/buddha.jpg"
+      );
       assert.typeOf(testEntity, "Object");
       assert.instanceOf(testEntity, BuddhistEntity);
-    });
-    it("has property id", function () {
-      assert.property(testEntity, "id");
-    });
-    it("has property name", function () {
-      assert.property(testEntity, "name");
-    });
-    it("has property religious_tradition", function () {
-      assert.property(testEntity, "religious_tradition");
-    });
-    it("has property image_url", function () {
-      assert.property(testEntity, "image_url");
     });
   });
   describe("#fetchEntries()", function () {
